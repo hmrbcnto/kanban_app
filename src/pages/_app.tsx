4c5f5ea4a53ff4b12cb2cbd22d303bf1755b2507
Homer <homer@avionschool.com>
2023-02-10 15:12:03 +0800

    wrapped App.tsx elements in ThemeContext

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,13 @@
+import ThemeContextWrapper from '@/context/theme';
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 
 const App = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />
+  return (
+    <ThemeContextWrapper>
+      <Component {...pageProps} />
+    </ThemeContextWrapper>
+  )
 }
 
 export default App;
